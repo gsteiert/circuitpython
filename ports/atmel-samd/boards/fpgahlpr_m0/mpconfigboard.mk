4511aac65ab820983c9057d49b6e0887d6393d9e
Greg Steiert <greg@steiert.net>
2018-09-01 01:29:08 +0000

    added new fpgahlpr_m0 board

---
+++ ports/atmel-samd/boards/fpgahlpr_m0/mpconfigboard.mk
@@ -0,0 +1,11 @@
+LD_FILE = boards/samd21x18-bootloader-external-flash-crystalless.ld
+USB_VID = 0x239A
+USB_PID = 0x8012
+USB_PRODUCT = "FPGA Helper"
+USB_MANUFACTURER = "Steiert Solutions"
+
+SPI_FLASH_FILESYSTEM = 1
+LONGINT_IMPL = MPZ
+
+CHIP_VARIANT = SAMD21E18A
+CHIP_FAMILY = samd21
